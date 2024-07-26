15d41e68a83f4e60e2f1fa00b61a50c0805a4811
Jatin <32764697+jatinphp@users.noreply.github.com>
2024-07-26 14:17:17 +0000

    destructing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
+import Practice from "./practice";
 
 function App() {
+  const { tesla, honda } = Practice;
+  const teslaTopSpeed = tesla.speedStats.topSpeed;
+  const hondaTopSpeed = honda.speedStats.topSpeed;
+  const teslaTopColour = tesla.coloursByPopularity[0];
+  const hondaTopColour = honda.coloursByPopularity[0];
+
   var options = { hour12: false };
   let localTime = new Date().toLocaleTimeString("en-US", options);
   const [time, setTime] = useState(localTime);
@@ -9,11 +16,31 @@ function App() {
     setTime(new Date().toLocaleTimeString("en-US", options));
   }
   setInterval(getTimeMethod, 1000);
+
   return (
     <div className="container">
       <h1>{time}</h1>
       <button onClick={getTimeMethod}>Get Time</button>
       <h2>{time}</h2>
+      <br />
+      <br />
+      <table>
+        <tr>
+          <th>Brand</th>
+          <th>Top Speed</th>
+          <th>Top color</th>
+        </tr>
+        <tr>
+          <td>{tesla.model}</td>
+          <td>{teslaTopSpeed}</td>
+          <td>{teslaTopColour}</td>
+        </tr>
+        <tr>
+          <td>{honda.model}</td>
+          <td>{hondaTopSpeed}</td>
+          <td>{hondaTopColour}</td>
+        </tr>
+      </table>
     </div>
   );
 }
